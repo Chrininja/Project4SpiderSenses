def21216e86b8e35b80bdb0aabf0bcc84b68dbf7
Christopher Lawrence (RIT Student) <cml3051@ad.rit.edu>
2018-12-03 16:25:44 -0500

    increased size of water

--- Project4SpiderSenses-1/Project4-SpiderSenses/js/classes.js
+++ Project4SpiderSenses-1/Project4-SpiderSenses/js/classes.js
@@ -45,7 +45,7 @@ class Bullet extends PIXI.Graphics {
     constructor(color = 0xFFFFFF, x = 0, y = 0) {
         super();
         this.beginFill(color);
-        this.drawRect(-2, -3, 4, 6);
+        this.drawRect(-40, -40, 40, 40);
         this.endFill();
         this.x = x;
         this.y = y;
@@ -54,7 +54,7 @@ class Bullet extends PIXI.Graphics {
             x: 0,
             y: -1
         };
-        this.speed = 400;
+        this.speed = 600;
         this.isAlive = true;
         Object.seal(this);
     }
